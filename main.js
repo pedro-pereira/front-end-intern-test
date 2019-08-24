703e3161d394bc67ba9cb6260a4c5a9b8b38ac6f
pedro-pereira <pedrohenripereira@gmail.com>
2019-08-24 16:19:37 -0300

    Primeira versão das validações JS
    Validação do tamanho dos inputs e formato de email.

--- main.js
+++ main.js
@@ -19,12 +19,37 @@
     // A propriedade "length" contém o tamanho (em caracteres) de uma
     // uma string. Toda string contém essa propriedade.
     const nameLength = nameValue.length;
+	
+	const emailValue = emailInput.value;
+	const emailLength = emailValue.length;
+	
+	const messageValue = messageInput.value;
+	const messageLength = messageValue.length;
+	
+	// Verifica se o usuário informou somente espaços em branco.
+	if(nameValue.trim().length <= 3) {
+		window.alert('O campo "nome" precisa ter no mínimo 3 caracteres.');
+	}
+	
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	if(emailValue.trim().length <= 3) {
+		window.alert('O campo "email" precisa ter no mínimo 3 caracteres.');
+	} else {
+		if (!filter.test(emailValue)) {
+			window.alert('O campo "email" deve estar no fomrato "email@example.com".');
+		}
+	}
+	
+	// Verifica se o usuário informou somente espaços em branco.
+	if(messageValue.trim().length <= 1) {
+		window.alert('O campo "mensagem" precisa estar preenchido.');
+	}
 
     // "window" é um objeto global que representa a janela (ou aba) do
     // navegador que está executando o código do seu site. O método
     // "alert" simplesmente mostra um aviso para o usuário contendo a
     // mensagem provida.
-    window.alert(`Botão enviar clicado! Conteúdo do campo nome: ${nameValue} (${nameLength} caracteres)`);
+    // window.alert(`Botão enviar clicado! Conteúdo do campo nome: ${nameValue} (${nameLength} caracteres)`);
 
     // Altere e complete essa função para validar os campos do formulário
     // de acordo com as especificações do teste. Boa sorte!
